9712755ab9ff00b94af12d0a1cf02b29d5f62c17
Jacob Nartey <jnartey@gmail.com>
2018-04-13 17:14:20 -0400

    Add files via upload

--- Cart_Assignment/src/cartSystemExample/CartSystem.java
+++ Cart_Assignment/src/cartSystemExample/CartSystem.java
@@ -1,6 +1,7 @@
 package cartSystemExample;
 
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,28 +11,27 @@ public class CartSystem extends TheSystem{
     public void display() {
       //Fill the code here
     	double total = 0;
-    	double subTotal = 0;
-    	double tax = 0;
     	int totalQuantity = 0;
+    	DecimalFormat df = new DecimalFormat("#.00");
     	
     	System.out.printf("%-25s | %-25s | %-15s | %-15s", "Item", "Description", "Quantity", "Sub Total");
 		System.out.println("");
 		System.out.println("--------------------------------------------------------------------------------");
 		
     	for(Map.Entry<String, Item> item : this.getItemCollection().entrySet()) {
-    		subTotal = item.getValue().getQuantity() * item.getValue().getItemPrice();
-    		tax = subTotal * 0.05;
+    		double subTotal = item.getValue().getQuantity() * item.getValue().getItemPrice();
+    		double tax = subTotal * 0.05;
     		
     		double subTotalTax = subTotal + tax;
     		total += subTotalTax;
     		totalQuantity += item.getValue().getQuantity();
     		
-    		System.out.printf("%-25s | %-25s | %-15s | %-15s", item.getKey(), item.getValue().getItemDesc(), item.getValue().getQuantity(), subTotalTax);
+    		System.out.printf("%-25s | %-25s | %-15s | %-15s", item.getKey(), item.getValue().getItemDesc(), item.getValue().getQuantity(), df.format(subTotalTax));
     		System.out.println("");
     	}
     	
     	System.out.println("--------------------------------------------------------------------------------");
-		System.out.printf("%-25s | %-25s | %-15s | %-15s", "", "", totalQuantity, total);
+		System.out.printf("%-25s  %25s  %-15s | %-15s", "", "Total", totalQuantity, df.format(total));
     	
     }
 }
