0cf78b2eb4ec963109e974b4a84ceb8e7d841f57
Jacob Nartey <jnartey@gmail.com>
2018-04-16 06:42:20 -0400

    Add files via upload

--- Cart_Assignment/src/cartSystemExample/CartSystem.java
+++ Cart_Assignment/src/cartSystemExample/CartSystem.java
@@ -14,25 +14,6 @@ public class CartSystem extends TheSystem{
     	int totalQuantity = 0;
     	DecimalFormat df = new DecimalFormat("#0.00");
     	
-//    	System.out.printf("%-25s | %-25s | %-15s | %-15s", "Item", "Description", "Quantity", "Sub Total");
-//		System.out.println("");
-//		System.out.println("--------------------------------------------------------------------------------");
-//		
-//    	for(Map.Entry<String, Item> item : this.getItemCollection().entrySet()) {
-//    		double subTotal = item.getValue().getQuantity() * item.getValue().getItemPrice();
-//    		double tax = subTotal * 0.05;
-//    		
-//    		double subTotalTax = subTotal + tax;
-//    		total += subTotalTax;
-//    		totalQuantity += item.getValue().getQuantity();
-//    		
-//    		System.out.printf("%-25s | %-25s | %-15s | %-15s", item.getKey(), item.getValue().getItemDesc(), item.getValue().getQuantity(), df.format(subTotalTax));
-//    		System.out.println("");
-//    	}
-//    	
-//    	System.out.println("--------------------------------------------------------------------------------");
-//		System.out.printf("%-25s  %25s  %-15s | %-15s", "", "Total", totalQuantity, df.format(total));
-		
     	if(this.getItemCollection().isEmpty()) {
     		System.out.println("Sub Total: " + 0.0);
     		System.out.println("Tax: " + 0.0);
@@ -50,6 +31,7 @@ public class CartSystem extends TheSystem{
         		System.out.println("Sub Total: " + df.format(subTotal));
         		System.out.println("Tax: " + df.format(tax));
         		System.out.println("Total: " + df.format(subTotalTax));
+        		System.out.println("");
         	}
     	}
     }
